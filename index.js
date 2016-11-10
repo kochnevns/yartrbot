e7fd16f30bb0e52f5a642cd38b15c916a258d1ae
ns kochnev <nikitulko@gmail.com>
2016-11-10 21:35:40 +0300

    username resolve via base class

--- index.js
+++ index.js
@@ -12,26 +12,27 @@ class YartrBot extends TelegramBot.TelegramBotBase {
         super(token);
         var self = this;
         self.bot.onText(/[a-zа-я]/gim, function(msg, match) {
-            log.info('Сообщение от @', msg.from.first_name, ' ', msg.from.last_name, '. Текст сообщения: ', '"', msg.text, '"')
+            log.info('Сообщение от ', self.getUsername(msg) ,'. Текст сообщения: ', '"', msg.text, '"')
             self.bot.sendMessage(msg.chat.id, '📡 Отправь свое местоположение');
         });
     }
     onLocation(locationMsg) {
         let stationKey = YartrUtils.getNearestStationKey(locationMsg.location),
             stationLinks = coordsMap[stationKey].links,
-        userLocationKeyboard = YartrUtils.generateLocationKeyboardOptions(stationLinks);
+            userLocationKeyboard = YartrUtils.generateLocationKeyboardOptions(stationLinks),
+            userName = this.getUsername(locationMsg);
 
-        log.info('Пришли координаты от @', locationMsg.from.first_name, ' ', locationMsg.from.last_name, '. Локация: ', '"', locationMsg.location, '"');
-        this.sendMessage(locationMsg.chat.id, `Ближе всего: ${stationKey}.`, userLocationKeyboard);
+        log.info('Пришли координаты от ', userName ,'. Локация: ', '"', locationMsg.location, '"');
+        this.bot.sendMessage(locationMsg.chat.id, `Ближе всего: ${stationKey}.`, userLocationKeyboard);
     }
     onCallbackQuery(msg) {
         let msgId = msg.from.id,
-            callbackParam = msg.data,
-            url = `http://yartr.ru/rasp.php?vt=1&nmar=78&q=1&id=${callbackParam}&view=2`;
+            url = `http://yartr.ru/rasp.php?vt=1&nmar=78&q=1&id=${msg.data}&view=2`,
+            userName = this.getUsername(msg);
 
-        this.answerCallbackQuery(msg.id, 'Ок, поехали!');
-        log.info('Начинаем обрабатывать сообщение от @', msg.from.first_name, ' ', msg.from.last_name, '. ссылка: ', '"', url, '"');
-        YartrResolve.resolveLink(url, this, msgId, msg);
+        this.bot.answerCallbackQuery(msg.id, 'Ок, поехали!');
+        log.info('Начинаем обрабатывать сообщение от ', userName, '. ссылка: ', '"', url, '"');
+        YartrResolve.resolveLink(url, this.bot, msgId, msg);
     }
 }
 const yarbot = new YartrBot(argvToken);
