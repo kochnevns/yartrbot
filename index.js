2578f8c4b0bf885d84365c3548bcdf77a73a1e9f
Nikita <kochnevns@icloud.com>
2017-06-26 13:24:09 +0300

    vse vishe i vishe

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 'use strict';
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 const
     TelegramBot = require('telegram-bot-oop-way'),
     YartrUtils = require('./lib/yartrUtils.js'),
@@ -7,18 +8,20 @@ const
     coordsMap = require('./stations/stations.json');
 
 class YartrBot extends TelegramBot {
-    constructor(token) {
+
+    constructor(token, coordsMap) {
         super(token);
         let self = this;
+        self.coordsMap = coordsMap;
         this.registerOnTextCallback(/[a-zа-я]/gim, self.ontextCb.bind(self));
     }
     onLocation(locationMsg) {
-        let stationKey = YartrUtils.getNearestStationKey(locationMsg.location),
-            stationLinks = coordsMap[stationKey].links,
-            userLocationKeyboard = YartrUtils.generateLocationKeyboardOptions(stationLinks),
+        let nearestStation = YartrUtils.getNearestStationKey(locationMsg.location),
+            nearestStationLinks = this.coordsMap[nearestStation].links,
+            userLocationKeyboard = YartrUtils.generateLocationKeyboardOptions(nearestStationLinks),
             userName = this.getUserName(locationMsg);
 
-        log.info('Пришли координаты от ', userName ,'. Локация: ', '"', locationMsg.location, '"');
+        log.info('Пришли координаты от ', userName, '. Локация: ', '"', locationMsg.location, '"');
         this.sendMessage(locationMsg.chat.id, `Ближе всего: ${stationKey}.`, userLocationKeyboard);
     }
     onCallbackQuery(msg) {
@@ -28,13 +31,14 @@ class YartrBot extends TelegramBot {
 
         this.answerCallbackQuery(msg.id, 'Ок, поехали!');
         log.info('Грабим расписание для ', userName, '. ссылка: ', '"', url, '"');
-        YartrUtils.resolveLink(url, this, msg).then( (message) => this.sendMessage(msgId, message));
+        YartrUtils.resolveLink(url, this, msg).then((answer) => this.sendMessage(msgId, answer));
     }
     ontextCb(msg) {
-      log.info('Сообщение от ', this.getUserName(msg) ,'. Текст сообщения: ', '"', msg.text, '"');
-      this.sendMessage(msg.chat.id, '📡 Отправь свое местоположение');
+        log.info('Сообщение от ', this.getUserName(msg), '. Текст сообщения: ', '"', msg.text, '"');
+        this.sendMessage(msg.chat.id, '📡 Отправь свое местоположение');
     }
+    
 }
 
 
-new YartrBot(argvToken);
+new YartrBot(argvToken, coordsMap);
